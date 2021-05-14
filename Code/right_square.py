8f7f1b200da8bc1ef8fd9256a7226887d213468f
khk1262 <khk1262@naver.com>
2021-05-15 04:06:55 +0900

    programmers gcd

---
+++ Code/right_square.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def solution(w,h):
+    return w * h - (w+h-gcd(w,h))
+
+w, h = map(int, input().split())
+
+print(solution(w,h))
